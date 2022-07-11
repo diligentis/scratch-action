e266ac3dd63da395b8f71129e5ea28fb68c22f84
diligentis <diligentis@users.noreply.github.com>
2022-07-11 16:40:33 -0400

    Adding in random stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
@@ -11,6 +12,26 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
+
+    let myOutput = ''
+    let myError = ''
+
+    const options: exec.ExecOptions = {}
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString()
+      },
+      stderr: (data: Buffer) => {
+        myError += data.toString()
+      }
+    }
+
+    const result = await exec.exec('git', ['diff', '--numstat'], options)
+    if (result !== 0) {
+      core.setFailed(myError)
+      return
+    }
+    core.info(myOutput)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
